3fd6e3da03ff691d6132e3ecab09394c124924b5
Jindrich Libovicky <jlibovicky@gmail.com>
2021-01-13 01:34:36 +0100

    test tokenizer

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -35,6 +35,9 @@ class TestCharTokenizer(unittest.TestCase):
         self.tokenizer = CharTokenizer(SPECIAL_SYMBOLS + list("ABCD"))
         self.abc_abcd_output = [[1, 4, 5, 6, 2, 0], [1, 4, 5, 6, 7, 2]]
 
+    def test_vocab_size(self):
+        self.assertEqual(self.tokenizer.vocab_size, 8)
+
     def test_batch_encode_plus_np(self):
         encoded = self.tokenizer.batch_encode_plus(
             ["ABC", "ABCD"], return_tensors="np")
@@ -62,8 +65,31 @@ class TestCharTokenizer(unittest.TestCase):
             ["ABC", "ABCD"],
             pad_to_max_length=False,
             return_attention_mask=False)
-        self.assertEqual(encoded[0].numpy().tolist(), self.abc_abcd_output[0][:-1])
+        self.assertEqual(
+            encoded[0].numpy().tolist(), self.abc_abcd_output[0][:-1])
         self.assertEqual(encoded[1].numpy().tolist(), self.abc_abcd_output[1])
+
+    def test_batch_encode_plus_types(self):
+        encoded = self.tokenizer.batch_encode_plus(
+            ["ABC", "ABCD"],
+            return_attention_mask=True)
+        self.assertEqual(encoded[0].dtype, torch.int64)
+        self.assertEqual(encoded[1].dtype, torch.float)
+
+    def test_decode(self):
+        decoded = self.tokenizer.batch_decode(self.abc_abcd_output)
+        self.assertEqual(decoded, ["ABC", "ABCD"])
+
+    def test_decode_np(self):
+        decoded = self.tokenizer.batch_decode(np.array(self.abc_abcd_output))
+        self.assertEqual(decoded, ["ABC", "ABCD"])
+
+    def test_decode_pt(self):
+        # pylint: disable=not-callable
+        decoded = self.tokenizer.batch_decode(
+            torch.tensor(self.abc_abcd_output))
+        # pylint: enable=not-callable
+        self.assertEqual(decoded, ["ABC", "ABCD"])
 
 
 if __name__ == '__main__':
