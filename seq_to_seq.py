7070a48df2a616a32aac4293a05169ab9966b7d7
Jindrich Libovicky <jlibovicky@gmail.com>
2021-02-09 11:06:11 +0100

    forgotten files

---
+++ seq_to_seq.py
@@ -0,0 +1,82 @@
+"""Encoder-decoder model."""
+
+from typing import List
+
+import torch
+import torch.nn as nn
+
+from lee_encoder import Encoder
+from decoder import Decoder, VanillaDecoder
+
+
+T = torch.Tensor
+
+class Seq2SeqModel(nn.Module):
+    def __init__(
+            self, vocab_size: int,
+            conv_filters: List[int],
+            nar_output: bool = False,
+            char_embedding_dim: int = 128,
+            dim: int = 512,
+            shrink_factor: int = 5,
+            highway_layers: int = 2,
+            ff_dim: int = None,
+            layers: int = 6,
+            attention_heads: int = 8,
+            dropout: float = 0.1,
+            vanilla_decoder: bool = False,
+            share_char_repr: bool = False) -> None:
+        super().__init__()
+
+        self.layers = layers
+
+        self.encoder = Encoder(
+            vocab_size=vocab_size,
+            char_embedding_dim=char_embedding_dim,
+            conv_filters=conv_filters,
+            dim=dim,
+            shrink_factor=shrink_factor,
+            highway_layers=highway_layers,
+            ff_dim=ff_dim, layers=layers,
+            attention_heads=attention_heads,
+            dropout=dropout)
+
+        if vanilla_decoder:
+            self.decoder = VanillaDecoder(
+                char_vocabulary_size=vocab_size,
+                dim=dim,
+                layers=layers,
+                ff_dim=ff_dim,
+                attention_heads=attention_heads,
+                dropout=dropout)
+        else:
+            self.decoder = Decoder(
+                char_vocabulary_size=vocab_size,
+                char_embedding_dim=char_embedding_dim,
+                conv_filters=conv_filters,
+                nar_output=nar_output,
+                dim=dim,
+                shrink_factor=shrink_factor,
+                highway_layers=highway_layers,
+                layers=layers,
+                ff_dim=ff_dim,
+                attention_heads=attention_heads,
+                dropout=dropout,
+                encoder=self.encoder if share_char_repr else None)
+
+    def forward(
+            self, src_batch: T, src_mask: T, tgt_batch: T, tgt_mask: T,
+            loss_function: nn.Module) -> T:
+        encoded, enc_mask = self.encoder(src_batch, src_mask)
+        loss = self.decoder(
+            encoded, enc_mask, tgt_batch, tgt_mask, loss_function)
+
+        return loss
+
+    @torch.no_grad()
+    def greedy_decode(self, src_batch: T, input_mask: T, eos_token_id: int, max_len: int = 100):
+        encoder_states, encoded_mask = self.encoder(src_batch, input_mask)
+        decoded, mask = self.decoder.greedy_decode(
+            encoder_states, encoded_mask, eos_token_id, max_len=max_len)
+
+        return decoded, mask
---
+++ train_translate.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+from typing import Iterable, Dict
+import random
+import sys
+
+import sacrebleu
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from char_tokenizer import from_data
+from seq_to_seq import Seq2SeqModel
+from lr_scheduler import NoamLR
+
+
+T = torch.Tensor
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+
+def preprocess_data(train_src, train_tgt, batch_size, tokenizer=None):
+    logging.info("Loading file %s.", train_src.name)
+    src_text = [line.strip() for line in train_src]
+    logging.info("Loading file %s.", train_tgt.name)
+    tgt_text = [line.strip() for line in train_tgt]
+
+    if tokenizer is None:
+        logging.info("Initializing tokenizer.")
+        tokenizer = from_data(src_text + tgt_text)
+
+    batches = []
+    src_batch, tgt_batch = [], []
+    total_sentences = 0
+    skipped = 0
+    logging.info("Binarizing and batching data.")
+    for src, tgt in zip(src_text, tgt_text):
+        total_sentences += 1
+        if len(src) > 300 or len(tgt) > 300:
+            skipped += 1
+            continue
+
+        src_batch.append(src)
+        tgt_batch.append(tgt)
+        if len(src_batch) >= batch_size:
+            batches.append((
+                tokenizer.batch_encode_plus(src_batch),
+                tokenizer.batch_encode_plus(tgt_batch)))
+            src_batch, tgt_batch = [], []
+    if src_batch:
+        batches.append((
+            tokenizer.batch_encode_plus(src_batch),
+            tokenizer.batch_encode_plus(tgt_batch)))
+
+    logging.info(
+        "Skipped %d sentences from %d in the dataset.",
+        skipped, total_sentences)
+    return tokenizer, batches
+
+
+@torch.no_grad()
+def validate(model, batches, loss_function, device, tokenizer):
+    model.eval()
+    loss_sum = 0
+    decoded_sentences = []
+    target_sentences = []
+    for i, ((src_data, src_mask), (tgt_data, tgt_mask)) in enumerate(batches):
+        src_data, src_mask = src_data.to(device), src_mask.to(device)
+        loss = model(
+            src_data, src_mask,
+            tgt_data.to(device), tgt_mask.to(device), loss_function)
+        loss_sum += loss
+        decoded_ids = model.greedy_decode(
+            src_data, src_mask, tokenizer.eos_token_id)[0]
+        decoded_sentences.extend(tokenizer.batch_decode(decoded_ids))
+        target_sentences.extend(tokenizer.batch_decode(tgt_data))
+
+        if i == 0:
+            for hyp, ref in zip(decoded_sentences[:5], target_sentences):
+                logging.info("'%s' -> '%s'", ref, hyp)
+
+    model.train()
+
+    bleu = sacrebleu.corpus_bleu(decoded_sentences, [target_sentences])
+
+    return loss_sum / len(batches), bleu.score
+
+
+def main():
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument(
+        "train_src", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "train_tgt", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "val_src", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "val_tgt", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "test_src", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument(
+        "test_src", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
+    parser.add_argument("--batch-size", type=int, default=512)
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--char-emb-dim", type=int, default=64)
+    parser.add_argument("--dim", type=int, default=512)
+    parser.add_argument("--layers", type=int, default=6)
+    parser.add_argument("--shrink-factor", type=int, default=5)
+    parser.add_argument("--nar-output", default=False, action="store_true")
+    parser.add_argument("--attention-heads", type=int, default=8)
+    parser.add_argument("--dropout", type=float, default=0.1)
+    parser.add_argument("--highway-layers", type=int, default=2)
+    parser.add_argument("--learning-rate", type=float, default=0.001)
+    parser.add_argument("--warmup", type=int, default=100)
+    parser.add_argument("--delay-update", type=int, default=1)
+    parser.add_argument("--validation-period", type=int, default=40)
+    parser.add_argument(
+        "--convolutions", nargs="+",
+        default=[200, 200, 250, 250, 300, 300, 300, 300],
+        type=int)
+    parser.add_argument(
+        "--vanilla-decoder", action="store_true", default=False)
+    args = parser.parse_args()
+
+    logging.info("Load and binarize data.")
+    tokenizer, train_batches = preprocess_data(
+        args.train_src, args.train_tgt, args.batch_size)
+    _, val_batches = preprocess_data(
+        args.val_src, args.val_tgt, args.batch_size, tokenizer=tokenizer)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info("Initializing model on device %s.", device)
+    model = Seq2SeqModel(
+        tokenizer.vocab_size,
+        conv_filters=args.convolutions,
+        char_embedding_dim=args.char_emb_dim,
+        dim=args.dim,
+        shrink_factor=args.shrink_factor,
+        nar_output=args.nar_output,
+        highway_layers=args.highway_layers,
+        ff_dim=2 * args.dim,
+        layers=args.layers,
+        attention_heads=args.attention_heads,
+        dropout=args.dropout,
+        vanilla_decoder=args.vanilla_decoder).to(device)
+
+    loss_function = nn.CrossEntropyLoss(reduction='none')
+    optimizer = optim.Adam(model.parameters())
+    scheduler = NoamLR(optimizer, args.warmup)
+
+    logging.info("Training starts.")
+    steps = 0
+    for epoch_n in range(args.epochs):
+        logging.info("Epoch %d starts, so far %d steps.", epoch_n + 1, steps)
+        for (src_data, src_mask), (tgt_data, tgt_mask) in train_batches:
+            steps += 1
+
+            loss = model(
+                src_data.to(device), src_mask.to(device),
+                tgt_data.to(device), tgt_mask.to(device), loss_function)
+
+            loss.backward()
+
+            if steps % args.delay_update == 0:
+                logging.info("Step %d, loss %.4g", steps, loss.item())
+                nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                optimizer.zero_grad()
+                scheduler.step()
+                torch.cuda.empty_cache()
+
+            if steps % args.validation_period == 0:
+                val_loss, val_bleu = validate(
+                    model, val_batches, loss_function, device, tokenizer)
+                logging.info(
+                    "VALIDATION: Step %d (epoch %d), loss %.4g, BLEU: %.4g",
+                    steps, epoch_n + 1, val_loss, val_bleu)
+        random.shuffle(train_batches)
+
+    logging.info("Done.")
+
+
+if __name__ == "__main__":
+    main()
